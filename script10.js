53e257a322944fe50d3937cb6f953c02457bbaaa
Gustavo Fernandes <95728805+igustavofernandes@users.noreply.github.com>
2022-06-14 23:30:08 -0300

    Add files via upload

---
+++ script10.js
@@ -0,0 +1,7 @@
+function saborSorvete(sabor){
+    if(sabor.toLowerCase() === "chocolate"){
+        alert("Amo sorvete de chocolate.")
+    }else {
+        alert("Prefiro outros sabores.")
+    }
+}
